a5c2b85bc7b21b696f42fa1e2d7dcf3ccd7012f3
ndegrate <ndegrate@pleaseassistme.co>
2023-07-30 22:00:09 -0500

    working db connection

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/blindbat/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
+	
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("DB_URL is not found in environment")
+	}
+	
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("PORT is not found in environment")
 	}
+	conn, err := sql.Open("postgres", db_url)
 
+	if err != nil {
+		log.Fatal("Can't connect to db")
+	}
+	queries, err := database.New(conn)
+	if err != nil {
+		log.Fatal()
+	}
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
